ed6f655af761321def0f2437c9763f36c7e624cd
sucaba <wareverbohdan@gmail.com>
2021-01-02 19:25:54 +0200

    feat: StreamableInContext and Contextual conforms to Streamable

--- src/streamable.rs
+++ src/streamable.rs
@@ -1,4 +1,5 @@
 use super::changable::Changable;
+use crate::contextual::Contextual;
 use crate::streaming::*;
 use std::error::Error;
 
@@ -7,10 +8,57 @@ pub trait Streamable: Changable {
     where
         S: Stream<Self::EventType>;
 
+    // TODO: Result<Vec<..>>
     fn take_changes(&mut self) -> Vec<Self::EventType> {
         let mut result = Vec::new();
         self.stream_to(&mut result).unwrap();
         result
+    }
+}
+
+/*
+impl<'a, T: Streamable> Streamable for &'a T {
+    fn stream_to<S>(self, stream: &mut S) -> Result<(), Box<dyn Error>>
+    where
+        S: Stream<Self::EventType> {
+        self.as_mut().stream_to(stream)
+    }
+}
+*/
+
+pub trait StreamableInContext<TCtx>: Changable {
+    fn stream_in_context_to<S>(
+        &mut self,
+        context: &mut TCtx,
+        stream: &mut S,
+    ) -> Result<(), Box<dyn Error>>
+    where
+        S: Stream<Self::EventType>;
+
+    fn take_changes_in_context(&mut self, context: &mut TCtx) -> Vec<Self::EventType> {
+        let mut result = Vec::new();
+        self.stream_in_context_to(context, &mut result).unwrap();
+        result
+    }
+}
+
+impl<T: Changable, TCtx> Changable for Contextual<T, TCtx> {
+    type EventType = T::EventType;
+
+    fn apply(&mut self, event: Self::EventType) -> Self::EventType {
+        self.subject.apply(event)
+    }
+}
+
+impl<T, TCtx> Streamable for Contextual<T, TCtx>
+where
+    T: StreamableInContext<TCtx>,
+{
+    fn stream_to<S>(&mut self, stream: &mut S) -> Result<(), Box<dyn Error>>
+    where
+        S: Stream<Self::EventType>,
+    {
+        self.subject.stream_in_context_to(&mut self.context, stream)
     }
 }
 
@@ -40,7 +88,7 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::contextual::*;
+    use crate::contextual::InContext;
     use pretty_assertions::assert_eq;
     use MyEvent::*;
 
@@ -49,16 +97,13 @@ mod tests {
     #[test]
     fn stream_in_context() {
         let entity_to_stream = MyEntity;
-        let context = MyContext { name: "exotic" };
+        let mut context = MyContext { name: "exotic" };
 
-        let mut sut = Contextual {
-            subject: entity_to_stream,
-            context,
-        };
+        let mut sut = entity_to_stream.in_context(context);
         let mut stream = Vec::new();
         let _ = sut.stream_to(&mut stream);
 
-        assert_eq!(stream, vec![ContextCaptured(MyContext { name: "exotic" })]);
+        assert_eq!(stream, vec![Captured(MyContext { name: "exotic" })]);
     }
 
     struct MyEntity;
@@ -66,7 +111,7 @@ mod tests {
     #[derive(Debug, Eq, PartialEq)]
     enum MyEvent {
         Dummy,
-        ContextCaptured(MyContext),
+        Captured(MyContext),
     }
 
     #[derive(Debug, Eq, PartialEq, Clone)]
@@ -74,18 +119,20 @@ mod tests {
         name: &'static str,
     }
 
-    impl Streamable for Contextual<MyEntity, MyContext> {
-        fn stream_to<S>(&mut self, stream: &mut S) -> Result<(), Box<dyn Error>>
+    impl StreamableInContext<MyContext> for MyEntity {
+        fn stream_in_context_to<S>(
+            &mut self,
+            context: &mut MyContext,
+            stream: &mut S,
+        ) -> Result<(), Box<dyn Error>>
         where
             S: Stream<Self::EventType>,
         {
-            stream.stream(vec![ContextCaptured(MyContext {
-                name: self.context.name,
-            })])
+            stream.stream(vec![Captured(MyContext { name: context.name })])
         }
     }
 
-    impl Changable for Contextual<MyEntity, MyContext> {
+    impl Changable for MyEntity {
         type EventType = MyEvent;
 
         fn apply(&mut self, _event: Self::EventType) -> Self::EventType {
