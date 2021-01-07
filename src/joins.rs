ea8d6e49a3f0de9d3614a8ba80bd843e9497d134
sucaba <wareverbohdan@gmail.com>
2021-01-07 22:45:29 +0200

    chore: join uses GetId instead of Identifiable

--- src/joins.rs
+++ src/joins.rs
@@ -1,6 +1,6 @@
 use itertools::{EitherOrBoth, Itertools};
 
-use crate::identifiable::{Identifiable, Id};
+use crate::identifiable::{GetId, Id, Identifiable};
 
 pub trait ManyReferences<OtherType: Identifiable> {
     type Iter: Iterator<Item = Id<OtherType>>;
@@ -12,20 +12,21 @@ pub trait SingleReference<OtherType: Identifiable> {
     fn reference(&self) -> Id<OtherType>;
 }
 
-pub fn join<'a, R, D, Refs, Defs>(
+pub fn join<'a, R, D, T, Refs, Defs>(
     refs: Refs,
     defs: Defs,
 ) -> impl IntoIterator<Item = (&'a R, &'a D)>
 where
     Refs: IntoIterator<Item = &'a R>,
     Defs: IntoIterator<Item = &'a D>,
-    R: 'a + SingleReference<D>,
-    D: 'a + Identifiable,
-    Id<D>: Ord,
+    R: 'a + SingleReference<T>,
+    D: 'a + GetId<IdentifiableType=T>,
+    T: Identifiable,
+    Id<T>: Ord,
 {
     refs.into_iter()
         .merge_join_by(defs, |x, y| {
-            Ord::cmp(&x.reference(), &y.id())
+            Ord::cmp(&x.reference(), &y.get_id())
         })
         .filter_map(|e| match e {
             EitherOrBoth::Both(x, y) => Some((x, y)),
