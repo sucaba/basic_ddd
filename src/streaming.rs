f43d7fe4d8df42bfd06fb9a983e5296fa0600e17
sucaba <wareverbohdan@gmail.com>
2021-06-02 10:24:10 +0300

    feat: fn pipe_streams()

--- src/streaming.rs
+++ src/streaming.rs
@@ -49,3 +49,29 @@ where
         self.0.stream(events.into_iter().map(&self.1))
     }
 }
+
+pub fn pipe_streams<S1, S2>(s1: S1, s2: S2) -> StreamsPipe<S1, S2> {
+    StreamsPipe { s1, s2 }
+}
+
+pub struct StreamsPipe<S1, S2> {
+    s1: S1,
+    s2: S2,
+}
+
+impl<TEvent, S1, S2> Stream<TEvent> for StreamsPipe<S1, S2>
+where
+    TEvent: Clone,
+    S1: Stream<TEvent>,
+    S2: Stream<TEvent>,
+{
+    fn stream<I>(&mut self, events: I) -> Result<usize, Box<dyn Error>>
+    where
+        I: IntoIterator<Item = TEvent>,
+    {
+        // TODO: Make it more memory efficient
+        let all: Vec<TEvent> = events.into_iter().collect();
+        self.s1.stream(all.clone())?;
+        self.s2.stream(all)
+    }
+}
